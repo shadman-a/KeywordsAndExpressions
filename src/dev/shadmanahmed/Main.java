9272b014ec5532ff86d2b345e3aa8a919aaf2608
shadman-a <shadman@outlook.com>
2020-11-20 13:32:13 -0500

    Initial commit

---
+++ src/dev/shadmanahmed/Main.java
@@ -0,0 +1,13 @@
+package dev.shadmanahmed;
+
+public class Main {
+
+    public static void main(String[] args) {
+    double kilometers = (100 * 1.60);
+    int highScore = 50;
+
+    if (highScore == 50) {
+        System.out.println("This is an expression " + kilometers);
+    }
+    }
+}
