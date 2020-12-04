5701236e26ecada25dbd8ba18c04d634fa3b9231
shadman-a <shadman@outlook.com>
2020-12-04 15:17:07 -0500

    passing arguments

--- src/dev/shadmanahmed/Main.java
+++ src/dev/shadmanahmed/Main.java
@@ -3,11 +3,17 @@ package dev.shadmanahmed;
 public class Main {
 
     public static void main(String[] args) {
-    double kilometers = (100 * 1.60);
-    int highScore = 50;
+        double kilometers = (100 * 1.60);
+        int highScore = 50;
 
-    if (highScore == 50) {
-        System.out.println("This is an expression " + kilometers);
+        if (highScore == 50) {
+            System.out.println("This is an expression " + kilometers);
+        }
+        calculateScore(highScore);
+
     }
+
+    public static void calculateScore(int highScore) {
+        System.out.println("Hello World " + highScore);
     }
 }
